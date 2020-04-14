d7bc66290fda2ae3a05081c98d68514b9cd5fee3
li-jingchen <lijcjc_xiaoming@163.com>
2020-04-14 17:39:00 +0800

    TWO

--- class-learn/learn-two.py
+++ class-learn/learn-two.py
@@ -3,10 +3,10 @@ class learn_2():
     #类变量
     a = 1
     b = 2
-    def __init__(self,a,b):
+    def __init__(self,a1,b1):
         #实例变量
-        self.a = a
-        self.b = b
+        self.a = a1
+        self.b = b1
         self.c = 1
         self.d = 2
     #类方法
@@ -19,9 +19,10 @@ class learn_2():
         print('这是个静态方法')
         print()
     #实例方法
-    def three(self,a,b):
-        ab = a*b
-        print(ab,self.c,self.d)
+    def three(self,a2,b2):
+        ab2 = a2*b2
+        #下面的self.c和self.d调用实例方法
+        print(ab2,self.c,self.d)
 if __name__ == '__main__':
     print(learn_2.one(1,2))
     learn_2.two()
@@ -29,5 +30,5 @@ if __name__ == '__main__':
     b = 4
     c = 5
     d = 10
-    # learn_2(a,b).three(a,b)
+    learn_2(a,b).three(a,b)
     learn_2(a,b).three(c,d)
